6c127d2baad7053330ed40dbe9f55b93fad00bd6
Joshua <joshmatimiranda@gmail.com>
2022-06-28 23:01:57 -0500

    began fetching data from api in utilities, worked on search function, and initialized scroll component for specific body parts

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
 
 const Exercises = () => {
   return <div>Exercises</div>;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,23 @@ import Exercises from "../components/Exercises";
 import SearchExercises from "../components/SearchExercises";
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
+  //needs to be reflected all across the app, not just in searchexercise comp
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercisees={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercisees={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </Box>
   );
 };
